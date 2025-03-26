890cdc5ab0c206ea42f55af9865fdefb5e4443ae
Tan Loc <daotanloc@gmail.com>
2025-03-26 15:29:22 +0700

    Khởi tạo dự án Windows Forms với MainForm

---
+++ ContactList/ContactList/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ContactList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblThem = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblThem
+            // 
+            this.lblThem.AutoSize = true;
+            this.lblThem.Location = new System.Drawing.Point(76, 56);
+            this.lblThem.Name = "lblThem";
+            this.lblThem.Size = new System.Drawing.Size(101, 13);
+            this.lblThem.TabIndex = 0;
+            this.lblThem.Text = "Danh Sách Liên Hệ";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(79, 168);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(121, 23);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm Liên Hệ";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.lblThem);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblThem;
+        private System.Windows.Forms.Button btnThem;
+    }
+}
+
