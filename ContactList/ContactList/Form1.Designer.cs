46e10c78e761efe357f67db0b94dfd3c754a2f8d
Nhat long <nhatlong@gmail.com>
2025-03-26 17:14:43 +0700

    Thêm Form để thêm liên hệ mới

--- ContactList/ContactList/Form1.Designer.cs
+++ ContactList/ContactList/Form1.Designer.cs
@@ -30,33 +30,48 @@
         {
             this.lblThem = new System.Windows.Forms.Label();
             this.btnThem = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblThem
             // 
             this.lblThem.AutoSize = true;
-            this.lblThem.Location = new System.Drawing.Point(76, 56);
+            this.lblThem.Location = new System.Drawing.Point(101, 69);
+            this.lblThem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblThem.Name = "lblThem";
-            this.lblThem.Size = new System.Drawing.Size(101, 13);
+            this.lblThem.Size = new System.Drawing.Size(122, 16);
             this.lblThem.TabIndex = 0;
             this.lblThem.Text = "Danh Sách Liên Hệ";
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(79, 168);
+            this.btnThem.Location = new System.Drawing.Point(105, 207);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(121, 23);
+            this.btnThem.Size = new System.Drawing.Size(161, 28);
             this.btnThem.TabIndex = 1;
             this.btnThem.Text = "Thêm Liên Hệ";
             this.btnThem.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(524, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "thêm liên hệ ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.lblThem);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "MainForm";
             this.ResumeLayout(false);
@@ -68,6 +83,7 @@
 
         private System.Windows.Forms.Label lblThem;
         private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
